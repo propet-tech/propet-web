e3a229c9a2cffccb602b443cb6b4c417716b5199
deividrx <galdinodeivid@gmail.com>
2022-10-26 18:57:18 -0300

    update some things

---
+++ src/app/pages/model/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+    constructor(
+        public id?: number,
+        public name?: string,
+        public status?: boolean,
+        public pet?: string,
+        public number?: number
+    ) {}
+
+}
